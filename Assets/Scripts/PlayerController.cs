0784142418efcf8ec2bdc3330609f6f15e4fd538
nguyennam <nguyensonnam1205@gmail.com>
2026-01-28 16:37:50 +0700

    add
    add

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,21 +7,26 @@ public class PlayerController : MonoBehaviour
     private PlayerInput inputActions;
 
     private CharacterController controller;
+    private Animator anim;
+    [SerializeField] private Transform cameraPos;
 
     [Header("*********Movement*********")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private Vector3 movement;
+    [SerializeField] private float rotationSpeed;
     [Header("Jump")]
     [SerializeField] private float JumpForce = 8f;
     [SerializeField] private float gravity = -9.81f;
     [SerializeField] private float gravityMultiplier = .5f;
     private Vector2 moveDirection;
+    private Vector2 lookDirection;
     [SerializeField]private float verticalVelocity;
 
     private void Awake()
     {
         inputActions = new PlayerInput();
         controller = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     private void OnEnable()
@@ -32,6 +37,8 @@ public class PlayerController : MonoBehaviour
 
         inputActions.Player.Move.performed += ctx => moveDirection = ctx.ReadValue<Vector2>();
         inputActions.Player.Move.canceled += ctx => moveDirection = Vector2.zero;
+
+        inputActions.Player.Look.performed += ctx => lookDirection = ctx.ReadValue<Vector2>();
 
         //jump
         inputActions.Player.Jump.performed += OnJump;
@@ -52,11 +59,23 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        movement = new Vector3(moveDirection.x, 0, moveDirection.y) * Time.deltaTime * moveSpeed;
+        
+        var lookRotation = Quaternion.Euler(0, cameraPos.eulerAngles.y, 0); 
+        var moveInputDirection = lookRotation * new Vector3(moveDirection.x, 0, moveDirection.y);
+        movement = moveInputDirection * Time.deltaTime * moveSpeed;
+
+        if(movement.magnitude > 0)
+        {
+            var t = rotationSpeed * Time.deltaTime;
+            Quaternion currentRotation = transform.rotation;
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            Quaternion nextRotation = Quaternion.Lerp(currentRotation, targetRotation, t);
+            transform.rotation = nextRotation;
+        }
 
         if(controller.isGrounded && verticalVelocity < 0)
         {
-            verticalVelocity = -2f;
+            verticalVelocity = -1f;
         }
         else
         {
@@ -65,12 +84,25 @@ public class PlayerController : MonoBehaviour
 
         movement.y = verticalVelocity * Time.deltaTime;
 
-        // Debug.Log("Move Direction: " + moveDirection);
-        if(movement.magnitude > 0.01f)
+        if(movement.magnitude > 0)
         {
             controller.Move(movement);
+            
         }
+
+        if(moveInputDirection.magnitude > 0)
+        {
+            anim.SetBool("Moving", true);
+        }
+        else
+        {
+            anim.SetBool("Moving", false);
+        }
+        
+        // anim.SetFloat("Speed", movement.normalized.magnitude);
     }
+
+    
 
     
 }
